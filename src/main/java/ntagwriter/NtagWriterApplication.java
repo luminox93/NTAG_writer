b060bc7203848effcf9f2f057841c00ea2d96a96
luminox93 <memoji1236@gmail.com>
2025-11-18 19:35:00 +0900

    chore: Java 21 업그레이드 및 기본 애플리케이션 설정

---
+++ src/main/java/ntagwriter/NtagWriterApplication.java
@@ -0,0 +1,8 @@
+package ntagwriter;
+
+public class NtagWriterApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+    }
+}
